f10e1bbefe23ef73ecec562db64a95b2e29afb69
Jesus Steer <jsteerv@gmail.com>
2022-03-14 12:55:50 -0500

    Created getInterest and cleaned comments

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -3,43 +3,68 @@ pragma solidity 0.8.0;
 
 import "./IBank.sol";
 
-/// @title Implementation of IBank interface
-/// @author Jesus Steer Varela
+/**
+* The purpose of this contract is to implement the IBank interface.
+* @title Bank contract.
+* @author Jesus Steer Varela.
+*/
 contract Bank is IBank {
     
     mapping (address => Account) private AccountBalance;
 
-    /// Transfer ether from account to the contract
-    /// @param amount the value to deposit
-    /// @dev increases the balance for the account
+    /**
+     * Return the accumulated interest for the given account.
+     * @dev calculate interest rate on deposits, which is 3% per 100 blocks.
+     * @return interest the calculated interest.
+     */
+    function getInterest() public view returns (uint256) {
+        uint256 accumulatedBlocks = block.number - AccountBalance[msg.sender].lastInterestBlock;
+        uint256 interest = (accumulatedBlocks * 3 * AccountBalance[msg.sender].deposit) / 10000;
+        return interest;
+    }
+
+    /**
+     * Transfer ether from account to the contract.
+     * @param amount the value to deposit.
+     * @dev increases the balance for the account.
+     */
     function deposit(uint256 amount) external payable override returns (bool) {
         require(msg.value == amount, "you can't contribute with a different amount");
         require(amount > 0, "you can't deposit 0 ether");
-        AccountBalance[msg.sender].deposit += amount;
+        AccountBalance[msg.sender].deposit += amount + getInterest();
         AccountBalance[msg.sender].lastInterestBlock = block.number;
         emit Deposit(msg.sender, amount);
         return true;
     }
 
-    /// Return the balance for an account.
-    /// @dev retrieves the value of the state variable `balance`
-    /// @return the stored balance
+    /**
+     * Return the balance for an account.
+     * @dev retrieves the value of the state variable `balance`.
+     * @return balance the stored balance.
+     */
     function getBalance() external view override returns (uint256) {
-        return AccountBalance[msg.sender].deposit;
+        uint256 balance = AccountBalance[msg.sender].deposit + getInterest();
+        return balance;
     }
 
-    /// Transfer ether from the contract to an account
-    /// @param amount the value to withdraw
-    /// @dev decreases the balance for the account
+    /**
+     * Transfer ether from the contract to an account.
+     * @param amount the value to withdraw.
+     * @dev decreases the balance for the account.
+     */
     function withdraw(uint256 amount) external override returns (uint256) {
+        if (amount == 0) {
+            amount = AccountBalance[msg.sender].deposit;
+        }
         require(AccountBalance[msg.sender].deposit != 0, "no balance");
-        require(AccountBalance[msg.sender].deposit > amount, "amount exceeds balance");
+        require(AccountBalance[msg.sender].deposit + getInterest() > amount, "amount exceeds balance");
+        amount += getInterest();
         address payable user = payable(msg.sender);
         require(user.send(amount), "withdrawal failed");
-        AccountBalance[msg.sender].deposit += amount;
+        AccountBalance[msg.sender].deposit += getInterest();
         AccountBalance[msg.sender].lastInterestBlock = block.number;
+        AccountBalance[msg.sender].deposit -= amount;
         emit Withdraw(msg.sender, amount);
         return 0;
     }
-
 }
